2b39bb65a473ff13905aad5f8df0993fbee77d31
OleksiiSytnik <oleksii.sytnik@student.uj.edu.pl>
2024-05-17 23:07:23 +0200

    cors fix

--- src/main/java/codibly/task/mainbackend/config/WebConfig.java
+++ src/main/java/codibly/task/mainbackend/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -14,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
